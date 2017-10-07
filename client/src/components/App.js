fb2ad658bfe9b51b47ea1495924b5779c9b4653a
Kihwan Cho <ozlongblack@gmail.com>
2017-10-07 23:10:00 +1100

    set up react-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Landing = () => <div>Landing</div>;
+const Dashboard = () => <div>Dashboard</div>;
+const SurveyNew = () => <div>SurveyNew</div>;
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
